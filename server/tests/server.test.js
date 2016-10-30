ffc9ec56dea8baa8ae6fb3381023681293dd6726
Rich Xu <richardxu100@gmail.com>
2016-10-30 11:14:39 -0400

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,55 @@
+const expect = require('expect');
+const request = require('supertest');
+
+const { app } = require('../server');
+const { Todo } = require('../models/todo');
+
+// lifecycle method
+beforeEach((done) => {
+  // wipes out all of the todos, so we start with 0 todos
+  Todo.remove({}).then(() => done());
+});
+
+describe('POST /todos', () => {
+  it('should create a new todo', (done) => {
+    var text = 'Test todo text';
+
+    request(app)
+      .post('/todos')
+      .send({ text }) // valid data
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.text).toBe(text);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err); // wraps up the test and prints err, stops anything below to run b/c return
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((err) => done(err));
+      });
+  });
+
+  it('should not create todo with invalid body data', (done) => {
+    var text = '';
+
+    request(app)
+      .post('/todos')
+      .send({ text })
+      .expect(400)
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(0);
+          done();
+        }).catch((err) => done(err));
+      });
+  });
+});
